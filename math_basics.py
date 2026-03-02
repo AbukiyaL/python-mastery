4827e7c73cf26330b13a10d6936369090f06efff
abukiya <highendcamry1@gmail.com>
2026-03-03 02:00:15 +0300

    complete math section

---
+++ math_basics.py
@@ -0,0 +1,16 @@
+# math_basics.py
+import math  # we can import math module to access buil-in functions like round(), ceil()... to work with numbers
+# we have the following arithmetic operators
+print(9 + 5)
+print(9 - 5)
+print(9 * 5)
+print(19 / 5)  # 3.8 (Division)
+print(13 // 4)  # 3 (Floor division - drops decimal)
+print(10 % 3)  # 1 (modulo refers to remainder)
+print(3 ** 2)  # 9 (refers to 3 raised to power of 2)
+
+y = 9
+y += 1  # 10 augmented assignment operator short form of y = y + 1
+print(y)
+
+print(math.ceil(2.5))  # 3 rounds up to 3
